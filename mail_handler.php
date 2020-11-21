91e819a60ba81a003b1a49dfa6b169e543597f66
FazlulAbid <69808044+FazlulAbid@users.noreply.github.com>
2020-11-21 21:20:11 +0530

    Update mail_handler.php

--- mail_handler.php
+++ mail_handler.php
@@ -1,39 +1,81 @@
+
 <?php
 	if(isset($_POST['submit'])){
 		$name=$_POST['name'];
 		$dob=$_POST['dob'];
-		$gender=$_POST['gender'];
+		$gender=$_POST['sex'];
+		// echo $gender;
+		
 		$age=$_POST['age'];
 		$grade=$_POST['grade'];
 		$email=$_POST['email'];
 		$phone=$_POST['phone'];
+		$alterphone=$_POST['alterphone'];
+		$currentlevel=$_POST['currentlevel'];
 		$place=$_POST['place'];
 		$country=$_POST['country'];
-		$course=$_POST['course'];
+		$parentname=$_POST['parentname'];		
 
-		$fathername=$_POST['fathername'];		
-		$mothername=$_POST['mothername'];
-
-		$parentphone=$_POST['fatherphone'];
-		$parentjob=$_POST['fatherjob'];
-
-
-
+		$parentphone=$_POST['parentphone'];
+		$parentjob=$_POST['parentjob'];
 		$siblingname1=$_POST['siblingname1'];
 		$siblingage1=$_POST['siblingage1'];
 		$siblingname2=$_POST['siblingname2'];
 		$siblingage2=$_POST['siblingage2'];
+		
+		
+		
+		$Vadic=false;
+		$Abacus=false;
+		$Personal=false;
+		$Handwriting=false;	
 
-		$to='fazalulabid007@gmail.com'; // Receiver Email ID, Replace with your email ID
-		$subject='Registration Form';
-		$message="Name :".$name."\n"."Date of Birth :".$dob."\n"."Gender :".$gender."\n"."Age :".$age."\n"."Grade :".$grade."\n"."Email :".$email."\n"."Phone Number :".$phone."\n"."Place :".$place."\n"."Country :".$country."\n"."Course :".$course."\n"."Father Name :".$fathername."\n"."Mother Name :".$mothername."\n"."Parent's Phone :".$parentphone."\n"."Parent's Occupation :".$parentjob."\n"."Sibling 1 Name:".$siblingname1."\n"."Sibling 1 Age :".$siblingage1."\n"."Sibling 2 Name :".$siblingname2."\n"."Sibling 2 Age :".$siblingage2."\n";
-		$headers="From: ".$email;
 
-		if(mail($to, $subject, $message, $headers)){
-			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
-		}
-		else{
-			echo "Something went wrong!";
-		}
+			
+		
+		
+		if(isset($_POST["Vadic"]))
+       {
+           $Vadic=TRUE; 
+            $Vadic = $_POST['Vadic'];
+           
+       }
+          
+       if(isset($_POST["Abacus"]))
+        {
+            $Abacus=TRUE;
+             $Abacus =  $_POST['Abacus']; 
+            
+        }
+       if(isset($_POST["Personal"]))
+        {
+            $Personal=TRUE;
+             $Personal =  $_POST['Personal'];
+        }
+		if(isset($_POST["Handwriting"]))
+        {
+            $Handwriting=TRUE;
+             $Handwriting =  $_POST['Handwriting'];
+        }
+    
+        $course = $Vadic." | ".$Abacus." | ".$Personal." | ".$Handwriting;
+
+        $to='fazalulabid007@gmail.com'; 
+        $subject='Registration Form';
+        $message="Name :".$name."\n"."Date of Birth :".$dob."\n"."Gender :".$gender."\n"."Age :".$age."\n"."Grade :".$grade."\n"."Email :".$email."\n"."Phone Number :".$phone."\n"."Phone Number (Optional):".$alterphone."\n"."Current Level (Existing Students Only):".$currentlevel."\n"."Place :".$place."\n"."Country :".$country."\n"."Course :".$course."\n"."Parent Name :".$parentname."\n"."Parent's Phone :".$parentphone."\n"."Parent's Occupation :".$parentjob."\n"."Sibling 1 Name:".$siblingname1."\n"."Sibling 1 Age :".$siblingage1."\n"."Sibling 2 Name :".$siblingname2."\n"."Sibling 2 Age :".$siblingage2."\n";
+        $headers="From: ".$email;
+    
+        if(mail($to, $subject, $message, $headers)){
+          echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+        }
+        else{
+          echo "Something went wrong!";
+        }
+
+
+
+		
+
+		
 	}
 ?>
